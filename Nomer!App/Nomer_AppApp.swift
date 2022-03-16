5d1abb30524157e26c99ccabab3c71232a32b2fb
iMtRo <maxmudovilyos75@gmail.com>
2022-03-16 19:22:32 +0500

    Initial Commit

---
+++ Nomer!App/Nomer_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nomer_AppApp.swift
+//  Nomer!App
+//
+//  Created by user on 16/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Nomer_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
